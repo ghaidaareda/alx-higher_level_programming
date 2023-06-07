2c78717c14fddc109cba01fac5c23dffdae09f67
ghaidaareda <dodo.sa7afy@gmail.com>
2023-06-07 16:21:58 -0700

    fizz_buzz

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    last_digits = []
+    for num in str(number):
+        last_digit = num % 10 
+        last_digit = last_digits.append(last_digit)
+    return last_digits
