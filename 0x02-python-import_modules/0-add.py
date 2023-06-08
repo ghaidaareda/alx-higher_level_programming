b6d4976c7071c7b01f55daa599edb5f5afcb76da
ghaidaareda <dodo.sa7afy@gmail.com>
2023-06-08 07:01:48 -0700

    pycodestyle check

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+from add_0 import add
 a = 1
 b = 2
-from add_0 import add
 if __name__ == '__main__':
     result = add(a, b)
     print("{a} + {b} = {result}".format(a=a, b=b, result=result))
