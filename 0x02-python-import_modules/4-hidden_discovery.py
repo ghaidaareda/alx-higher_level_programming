8c262feb953a4d00dd2e468153273fb66282bca2
ghaidaareda <dodo.sa7afy@gmail.com>
2023-06-08 13:55:51 -0700

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,5 +2,7 @@
 import hidden_4
 if  __name__ == '__main__':
     names = [name for name in dir(hidden_4) if not name.startswith('__')]
-    print(names)
+    names_sorted = sorted(names)
+    for name in names_sorted
+    print(name)
 
