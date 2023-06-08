4849837672312a68968a4eb0ed8470e7a33a5e83
ghaidaareda <dodo.sa7afy@gmail.com>
2023-06-08 06:59:28 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,5 +2,6 @@
 a = 1
 b = 2
 from add_0 import add
-result = add(a, b)
-print("{a} + {b} = {result}".format(a=a, b=b, result=result))
+if __name__ == '__main__':
+    result = add(a, b)
+    print("{a} + {b} = {result}".format(a=a, b=b, result=result))
