9798a3d375320de37c85a93af1d118cf5185160c
ghaidaareda <dodo.sa7afy@gmail.com>
2023-06-10 16:51:19 -0700

    max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        for i in my_list:
+            my_list.sort()
+            biggest = my_list[-1]
+            return(biggest)
