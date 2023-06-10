810f8afa252fdb9dae81ac0b9af89a3556a5bfca
ghaidaareda <dodo.sa7afy@gmail.com>
2023-06-10 15:58:36 -0700

    no_c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     if my_string:
         new_list = list(my_string)
-        for i in new_list.copy():
+        for i in new_list:
             if i == "c" or i == "C":
                 new_list.remove(i)
         return("".join(new_list))
