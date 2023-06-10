5107bd290013b4555343ca7fbec914a6a08ad8f0
ghaidaareda <dodo.sa7afy@gmail.com>
2023-06-10 16:43:20 -0700

    matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for raw in matrix:
         for col in raw:
-            print("{:d}".format(col), end=" " if col != raw [- 1] else "")
+            print("{:d}".format(col), end=" " if col != raw[- 1] else "")
         print()
