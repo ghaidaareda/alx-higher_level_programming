67d646988d9c24d122a588fc211546df17c40273
ghaidaareda <dodo.sa7afy@gmail.com>
2023-06-10 17:21:12 -0700

    not_c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@ def no_c(my_string):
     if my_string:
         new_string = ""
         for i in my_string:
-            if i not in "cC":
+            if i != "c" and i != "C":
                 new_string += i
         return new_string
