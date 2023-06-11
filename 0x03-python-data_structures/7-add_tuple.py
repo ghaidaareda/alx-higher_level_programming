9530f9463fd25d24ec18ce23854519ef5088997d
ghaidaareda <dodo.sa7afy@gmail.com>
2023-06-11 13:29:46 -0700

    add_tple_update

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -17,4 +17,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_c = (tuple_b[0] + 0, 0)
     elif len(tuple_a) == 1 and len(tuple_b) == 0:
         tuple_c = (tuple_a[0] + 0, 0)
-    return(tuple_c)
+    return tuple_c
