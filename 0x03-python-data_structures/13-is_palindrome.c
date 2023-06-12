337b01ac160b394697f2f4d7dea7f7cb855b65b2
ghaidaareda <dodo.sa7afy@gmail.com>
2023-06-12 08:10:10 -0700

    is_palindrome_head

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -12,12 +12,10 @@ int is_palindrome(listint_t **head)
 	listint_t *fast = *head;
 	listint_t *middle, *second_half, *current, *prev, *node1, *node2;
 	
-	if (head  == NULL)
+	if (*head  == NULL)
 	{
 		return(1);
 	}
-	else
-	{
 	while (fast != NULL && fast->next != NULL)
 	{
 		slow = slow->next;
@@ -46,5 +44,4 @@ int is_palindrome(listint_t **head)
 		node2 = node2->next;
 	}
 	return (1);
-	}
 }
