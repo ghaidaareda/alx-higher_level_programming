d1e60bd922a9a244a9febd797d839d4dcae90b0c
ghaidaareda <dodo.sa7afy@gmail.com>
2023-06-26 13:36:15 -0700

    task2

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,9 +2,11 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
-        while count < x:
-            print(my_list[count], end="")
+        for i in my_list:
+            print(i, end="")
             count += 1
+            if count == x:
+                break
     except IndexError:
         pass
     finally:
---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    i = 0
+    while i < x:
+        try:
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
+                count += 1
+            i += 1
+        except Exception:
+            raise IndexError("list index out of range")
+    print()
+    return count
