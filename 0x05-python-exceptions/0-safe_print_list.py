d968782d84229678bbb5c60c96c47c4b46db1673
ghaidaareda <dodo.sa7afy@gmail.com>
2023-06-26 09:08:45 -0700

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for i in my_list:
+        try:
+            print("{}".format(i), end="")
+            count += 1
+            if x == count:
+                break
+        except TypeError:
+            break
+    print()
+    if x > count:
+        return count
+    else:
+        return x
