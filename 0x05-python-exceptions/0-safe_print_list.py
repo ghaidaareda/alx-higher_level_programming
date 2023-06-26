39f369384d19dee44360abe078f706d80ee63959
ghaidaareda <dodo.sa7afy@gmail.com>
2023-06-26 09:14:30 -0700

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,7 +8,7 @@ def safe_print_list(my_list=[], x=0):
             if x == count:
                 break
         except TypeError:
-            break
+            pass
     print()
     if x > count:
         return count
