21fcdf551233c84f995ef6d65a66fff396407c4a
ghaidaareda <dodo.sa7afy@gmail.com>
2023-06-26 11:13:35 -0700

    safe_print_update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     for i in my_list:
         try:
-            print(i, end="")
+            print("{}".format(i), end="")
             count += 1
             if x == count:
                 break
