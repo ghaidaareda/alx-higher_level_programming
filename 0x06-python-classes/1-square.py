08c2057f7e1b9145bef744397fe9984d433e0d6a
ghaidaareda <dodo.sa7afy@gmail.com>
2023-06-28 14:57:32 -0700

    size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" class Square that defines a square """
+
+
+class Square:
+    """ empty class represtents a square """
+    def __int__(self, size):
+        """
+        class Square that defines a square
+        size : Private instance attribute
+        """
+        self.__size = size
