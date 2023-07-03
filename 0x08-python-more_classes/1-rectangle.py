1bad732a59a93712830ad08cef20e22685a74cfb
ghaidaareda <dodo.sa7afy@gmail.com>
2023-07-03 06:55:48 -0700

    1-rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""class that defines a rectangle"""
+
+
+class Rectangle:
+    """ class defines rectangle"""
+    def __init__(self, width=0, height=0):
+        """ 
+        init method define square
+        width:private instance define width
+        height:private instance define height
+        """
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        elif not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__width = width
+            self.__height = height
+
+    @property
+    def width(self):
+        """getter"""
+        self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """getter"""
+        self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
