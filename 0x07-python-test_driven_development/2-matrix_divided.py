eae760c2a5b5be279202520a35ff3c8b718ed744
ghaidaareda <dodo.sa7afy@gmail.com>
2023-07-05 16:01:01 -0700

    matrix_div

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" matrix_divided function"""
+def matrix_divided(matrix, div):
+    new_matrix = []
+    msg = "matrix must be a matrix (list of lists) of integers/floats"
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    elif not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError("div must be a number")
+    elif not isinstance(matrix, list):
+        raise TypeError(msg)
+    else:
+        len_first_sublist = len(matrix[0])
+        if not all(len(sublist) == len_first_sublist for sublist in matrix):
+            raise TypeError("Each row of the matrix must have the same size")
+        for sublist in matrix:
+            raw =[]
+            for i in sublist:
+                if not isinstance(sublist, list):
+                    raise TypeError(msg)
+                elif not isinstance(i, int) and not isinstance(i, float):
+                    raise TypeError(msg)
+                result = round(float(i / div), 2)
+                raw.append(result)
+            new_matrix.append(raw)
+        return new_matrix
+
+
