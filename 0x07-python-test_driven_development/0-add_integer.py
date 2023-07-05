c9e6c7849600fa568a667146bcb2f933808dd7b5
ghaidaareda <dodo.sa7afy@gmail.com>
2023-07-05 08:47:04 -0700

    add

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" add function and its doctest"""
+
+
+def add_integer(a, b=98):
+    """ 
+    function to add integers
+    consider float as integer
+    a,b are arguments
+    if b not exist it is 98 by default
+    """
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    elif isinstance(a, float):
+        a = int(a)
+    elif isinstance(b, float):
+        b = int(b)
+    else:
+        return a + b
