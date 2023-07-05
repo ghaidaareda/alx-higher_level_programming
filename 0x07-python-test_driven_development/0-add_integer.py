ce20383f2909d8c4716def7ae70d2351cb582355
ghaidaareda <dodo.sa7afy@gmail.com>
2023-07-05 13:55:36 -0700

    update

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -14,10 +14,12 @@ def add_integer(a, b=98):
             a = int(a)
         elif not isinstance(a, int) and not isinstance(a, float):
             raise TypeError("a must be an integer")
+        elif a is None:
+            raise TypeError("add_integer() missing 1 required positional argument: 'a'")
         elif isinstance(b, float):
             b = int(b)
         elif not isinstance(b, int) and not isinstance(b, float):
             raise TypeError("b must be an integer")
-        return a + b
+        return int(a + b)
     except TypeError as e:
         raise TypeError(str(e))
