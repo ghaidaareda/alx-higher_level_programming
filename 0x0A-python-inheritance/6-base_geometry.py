2002ec769c7295304e7f96e298e49763c248424f
ghaidaareda <dodo.sa7afy@gmail.com>
2023-07-10 15:14:26 -0700

    6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""BaseGeometry"""
+
+
+class BaseGeometry:
+    """class BaseGeometry"""
+    def area(self):
+        """ Public instance method """
+        raise Exception("area() is not implemented")
