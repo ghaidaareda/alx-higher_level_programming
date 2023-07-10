f89868d48c4ffa33dc6264dc6cc1c5f34819dfc9
ghaidaareda <dodo.sa7afy@gmail.com>
2023-07-10 14:26:33 -0700

    si instance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ check if the object is exactly an instance of the specified class """
+
+
 def is_same_class(obj, a_class):
     """ check if the object is exactly an instance of the specified class """
     return type(obj) == a_class
