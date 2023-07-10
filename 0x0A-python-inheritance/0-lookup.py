04e49344b0cccc9f58e45e996d844da47f4189f5
ghaidaareda <dodo.sa7afy@gmail.com>
2023-07-10 13:45:54 -0700

    1-my_list.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """ returns the list of available attributes"""
 
+
 def lookup(obj):
     """
-    function that returns the list of available 
+    function that returns the list of available
     attributes and methods of an object
     """
     return dir(obj)
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" class MyList that inherits from list """
+
+
+class MyList(list):
+    """
+    Public instance method
+    that prints the list, but sorted (ascending sort)
+    """
+    def print_sorted(self):
+        print(sorted(self))
