0839905179e3e8953e3679ec4f4f5fcbb9a011d8
ghaidaareda <dodo.sa7afy@gmail.com>
2023-07-13 05:55:00 -0700

    1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-function that  writes text file (UTF8) and prints it to stdout
+function that  writes text file (UTF8) and returns the number of characters
 """
 
 
 def write_file(filename="", text=""):
     """
-    function that reads a text file (UTF8) and prints it to stdout
+    function that writes a string to a text file
     """
-    with open(filename, "r", encoding="utf-8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
         
