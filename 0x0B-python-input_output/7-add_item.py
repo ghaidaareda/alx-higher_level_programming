e8aa6ba89fc2ff73092f96b2ac408a4652e91f36
ghaidaareda <dodo.sa7afy@gmail.com>
2023-07-15 06:21:38 -0700

    pycode

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,7 +11,7 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 args = sys.argv[1:]
 try:
     my_list = load_from_json_file("add_item.json")
-except:
-    my_list =[]
+except Exception:
+    my_list = []
 my_list.extend(args)
 save_to_json_file(my_list, "add_item.json")
