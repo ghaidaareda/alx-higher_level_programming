3beeb025d2ffa5298095d1304b7d8308038018be
ghaidaareda <dodo.sa7afy@gmail.com>
2023-07-15 04:52:57 -0700

    base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" class Base """
+class Base:
+    """
+    class Base
+    “base” of all other classes in this project
+     to manage id attribute in all future classes
+    """
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """ class constructor"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
