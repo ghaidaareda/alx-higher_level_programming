d7fcd56f90fca6c27f5277125872a3302775607a
ghaidaareda <dodo.sa7afy@gmail.com>
2023-08-16 10:15:34 -0700

    erreor

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,9 +1,2 @@
 -- creates the database hbtn_0c_0
-IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = 'hbtn_0c_0')
-	BEGIN
-		CREATE DATABASE hbtn_0c_0
-	END
-ELSE 
-	BEGIN 
-    		REPLACE hbtn_0c_0 
-	END
+CREATE DATABASE hbtn_0c_0
