4804c7759a1bbd5436857a9ad007df1c1581c4e2
ghaidaareda <dodo.sa7afy@gmail.com>
2023-09-15 21:09:25 +0300

    0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+script that lists all states from the database hbtn_0e_0_usa
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    query = "SELECT * FROM states ORDER BY states.id ASC"
+    cursor.execute(query)
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
+    cursor.close()
+    db.close()
