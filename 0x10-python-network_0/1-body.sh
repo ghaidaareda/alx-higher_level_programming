1662ab2d6aa994db86d4654f76d4b8b66e8a213f
ghaidaareda <dodo.sa7afy@gmail.com>
2023-09-29 02:26:08 +0300

    1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 #script that Display only body of a 200 status code response
-response=$(curl -s -o /dev/null -w "%{http_code}" "$url")
-
-curl -s -X GET "$1" -o /dev/null -w "%{http_code}":wq
- | grep -q 200 && curl -s "$1"
+curl -s -X GET "$1" -L 200
