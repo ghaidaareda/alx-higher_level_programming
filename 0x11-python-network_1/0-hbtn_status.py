29d012cad448680e85451984a70532bf7387c226
ghaidaareda <dodo.sa7afy@gmail.com>
2023-09-29 19:45:15 +0300

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,10 +5,8 @@ script that fetches https://alx-intranet.hbtn.io/status
 import urllib.request
 import requests
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        html = response.read()
-        response = requests.get('https://alx-intranet.hbtn.io/status')
-        print("Body response:")
-        print("\t- type:", type(html))
-        print("\t- content:", response.content)
-        print("\t- utf8 content:", response.content.decode('utf8'))
+    response = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type:", type(html))
+    print("\t- content:", response.content)
+    print("\t- utf8 content:", response.content.decode('utf8'))
