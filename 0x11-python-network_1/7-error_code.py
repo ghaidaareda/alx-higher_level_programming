13b16ce17732b2777ea8f6d89d62b35e62e6dee7
ghaidaareda <dodo.sa7afy@gmail.com>
2023-09-29 18:48:06 -0700

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -10,7 +10,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
-    try:
-        print(response.text)
-    except response.status_code >= 400 as e:
+    if (response.status_code > 400):
         print("Error code:", response.status_code)
+    else:
+        print(response.text)
