8c23938d1408454902154fe7f34bdd57a936e0c2
ghaidaareda <dodo.sa7afy@gmail.com>
2023-09-30 23:37:38 +0300

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -11,6 +11,5 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     mail = {"email": sys.argv[2]}
-    encoded_email = urlencode({"email": email})
     req = requests.post(url, data=encoded_email)
     print(req.text)
