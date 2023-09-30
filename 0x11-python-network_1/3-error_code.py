6ee6175c4bf5743c51b44560272ec3a233395d4f
ghaidaareda <dodo.sa7afy@gmail.com>
2023-09-29 17:58:55 -0700

    1

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,7 +6,7 @@ and displays the body of the response (decoded in utf-8).
 """
 import urllib.request
 import sys
-import urllib.error
+from urllib.error import URLError, HTTPError
 
 if __name__ == "__main__":
     url = sys.argv[1]
