5a31ed013289a3a5713e66a7a9f127c3d6f1fe02
ghaidaareda <dodo.sa7afy@gmail.com>
2023-09-30 22:39:47 +0300

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -2,17 +2,14 @@
 """
 script that takes in a URL & email,
 sends a POST request to the URL with email
-and displays the  body of the response (decoded in utf-8)
+and displays the  body of the response.
+usin requests lib.
 """
-import urllib.request
+import requests
 import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
     mail = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(mail)
-    data = data.encode('ascii')
-    req = urllib.request.Request(url, data)
-    with urllib.request.urlopen(req) as response:
-        content = response.read().decode('utf8')
-        print(content)
+    req = Requests.post(url, data=mail)
+    print(req.text)
