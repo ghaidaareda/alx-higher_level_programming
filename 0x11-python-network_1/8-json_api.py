ccc0780cef4d05d0d9344b02263f65d4c9201a59
ghaidaareda <dodo.sa7afy@gmail.com>
2023-10-01 01:08:16 +0300

    8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     try:
         json_response = response.json()
         if json_response:
-            print(f"[{json_response.get(id)}] {json_response.get(name)}")
+            print(f"[{json_response.get('id')}] {json_response.get('name')}")
         else:
             print("No result")
     except ValueError:
