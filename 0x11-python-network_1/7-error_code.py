662999d2a3482be6d95153fd621473d6270e9e83
ghaidaareda <dodo.sa7afy@gmail.com>
2023-09-30 04:39:39 +0300

    7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL,
+sends a request to the URL
+and displays the body of the response (decoded in utf-8).
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    try:
+        decoded_response = content.response('utf-8')
+        print(decoded_response)
+    except response.status_code >= 400 as e:
+        print("Error code:", response.status_code)
